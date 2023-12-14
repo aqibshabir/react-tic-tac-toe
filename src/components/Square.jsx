a51ef4d0d3830247d86f3abe1171e07ce011d7a2
aqibshabir <aqibfaisalshabir@gmail.com>
2023-12-14 11:33:58 +0000

    added state

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
