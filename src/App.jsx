00ce9a1f81f8dc5b98eabe87320af21755643bc7
aqibshabir <aqibfaisalshabir@gmail.com>
2023-12-14 11:26:05 +0000

    basic board with sqaure components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
-import React from "react";
+import Square from "./components/Square";
 
-function Square() {
-  return <button className="square">X</button>;
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 }
 
-export default Square;
+export default Board;
